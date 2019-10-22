bc2a27f2548c760423cb088077e2a4be49da7323
DaseulChun <nandaseul@gmail.com>
2019-10-21 20:40:57 -0700

    change sign_in/sign_up design and use simple_form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email params[:email]
+    @user = User.find_by_email params[:sessions][:email]
 
-    if @user&.authenticate(params[:password])
+    if @user&.authenticate(params[:sessions][:password])
       session[:user_id] = @user.id
       redirect_to root_path, notice: "Logged In"
     else
