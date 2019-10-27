5f7e6bbbc5c6b9e76d8a83c7b2b743482165e9d8
DaseulChun <nandaseul@gmail.com>
2019-10-26 22:06:06 -0700

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Product.destroy_all
+LineItem.destroy_all
+Cart.destroy_all
+Payment.destroy_all
+
+PASSWORD = "supersecret"
+
+admin_user = User.create(
+  first_name: "Admin",
+  last_name: "User",
+  email: "admin.user@example.com",
+  password: PASSWORD,
+  is_admin: true
+)
+
+customer_user = User.create(
+    first_name: "Customer",
+    last_name: "User",
+    email: "customer.user@example.com",
+    password: PASSWORD,
+    is_admin: false
+)
