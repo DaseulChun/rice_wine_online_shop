6702275a2df501fb00af1c3823cea481068a2a06
DaseulChun <nandaseul@gmail.com>
2020-07-22 16:42:17 -0700

    Implement closing paymend modal when clicking background overlay

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,11 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const button = document.getElementById('check-out-button');
   const modal = document.getElementById('payment-modal');
   const close = document.getElementById('modal-close');
+  const modalBackground = document.querySelector('.modal-background');
 
   button.onclick = function() {
     modal.style.display = 'flex';
   }
+
   close.onclick = function() {
+    modal.style.display = 'none';
+  }
+  
+  modalBackground.onclick = function() {
     modal.style.display = 'none';
   }
 })
