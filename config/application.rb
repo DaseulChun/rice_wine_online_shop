a609630d6099c51969534637e7288a161fdb4e5d
DaseulChun <nandaseul@gmail.com>
2020-07-24 12:33:12 -0700

    Precompile locally for production, remove unused image files

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,6 @@ module RiceWineOnlineShop
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    # config.serve_static_assets = true
+    config.serve_static_assets = true
   end
 end
